dc57663349197a74d6beeaecbd2baad8fbb50940
Jatindera Walia <jatindera_walia@hotmail.com>
2019-11-21 18:04:44 -0600

    dynamic forms on ng.io

---
+++ src/app/question-drop-down.spec.ts
@@ -0,0 +1,7 @@
+import { QuestionDropDown } from './question-drop-down';
+
+describe('QuestionDropDown', () => {
+  it('should create an instance', () => {
+    expect(new QuestionDropDown()).toBeTruthy();
+  });
+});
---
+++ src/app/question-text-box.spec.ts
@@ -0,0 +1,7 @@
+import { QuestionTextBox } from './question-text-box';
+
+describe('QuestionTextBox', () => {
+  it('should create an instance', () => {
+    expect(new QuestionTextBox()).toBeTruthy();
+  });
+});
